71c9b593e1a79cf0413cd9d628197a9c2faa483c
Ethel Ng <ethel.ng@gmail.com>
2014-09-04 19:34:40 +0100

    Attempt to replace session tokens for player names'

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -16,34 +16,25 @@ class Battleships < Sinatra::Base
 	end
 
 	get '/new_game' do
-		# @players = session[:game_players]
-		@player_one = session[:player_one_name]
-		@player_two = session[:player_two_name]
 		erb :new_game
 	end
 
 	post '/new_game/submit/player/:id' do |number|
-		player_name = ("player_" + number + "_name").to_sym
-		session[player_name] = params[:player]
 		this_player = Player.new
 		session[:player_id] = this_player.object_id
 		GAME.add_player(this_player)
-		# session[:game_players] ||= []
-		# session[:game_players] << this_player
 		redirect '/new_game'
 	end
 
 	delete '/new_game/reset' do
 		session.clear
-		#GAME.reset
+		GAME.reset
 		redirect '/'
 	end
 
 	get '/ship_placement' do
-		@players = GAME.players
 		GAME.set_up_boards
-		# below is not working at present because the players inside the cookie have not been updated when set_up_boards was called
-		@board = @players.first.ship_board.grid
+		@board = GAME.current_player.ship_board.grid
 		erb :ship_placement
 	end
 
